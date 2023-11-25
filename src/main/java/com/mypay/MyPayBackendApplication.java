d0f714aae56985be119873af04650504a7b8191d
Devendra <ndevendra1310@gmail.com>
2023-11-25 17:07:06 +0530

    1st commit

---
+++ src/main/java/com/mypay/MyPayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mypay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPayBackendApplication.class, args);
+	}
+
+}
